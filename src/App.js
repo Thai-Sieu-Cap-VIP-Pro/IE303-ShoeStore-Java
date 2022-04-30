9fd6baa5acce7c6acf1f86b1d1d2dea8e7f471eb
Le Duc Thang <19522197@gm.uit.edu.vn>
2022-04-30 12:46:13 +0700

    thang commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { Outlet, Route, Routes } from "react-router-dom";
 import AuthIndex from "./features/auth/AuthIndex";
-
+import Admin from "./features/admin/pages/Admin";
+import ProductManager from "./features/admin/pages/ProductManager";
+import BrandManager from "./features/admin/pages/BrandManager";
+import OrderManager from "./features/admin/pages/OrderManager";
+import Report from "./features/admin/pages/Report";
+import './App.css'
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/auth/*" element={<AuthIndex />} />
+        <Route path="/auth" element={<AuthIndex />}/>
+        <Route path="/admin/*" element={<Admin/>}>
+          <Route path="product" element={<ProductManager/>} />
+          <Route path="brand" element={<BrandManager/>} />
+          <Route path="order" element={<OrderManager/>} />
+          <Route path="report" element={<Report/>} />
+        </Route>
       </Routes>
     </div>
   );
