7039e1528241cf7a0d321a752510a2b81b976cb5
Ngô Gia Thái <81607294+Thai-Sieu-Cap-VIP-Pro@users.noreply.github.com>
2022-05-05 14:22:02 +0700

    Thai update login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Outlet, Route, Routes } from "react-router-dom";
+import Auth from "./features/admin/auth";
 import AuthReducer from "./features/customer/auth/AuthSlice";
 import Example from "./shareComponent/formikCustom/example";
 
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <Example />
       <Routes>
-        <Route path="/auth/*" element={<AuthReducer />} />
+        <Route path="/admin/*" element={<Auth />} />
       </Routes>
     </div>
   );
