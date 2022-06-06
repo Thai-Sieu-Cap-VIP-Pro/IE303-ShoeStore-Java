69fa9de6036d36951e3ac8121f4e3440fcb5a480
Ngô Gia Thái <81607294+Thai-Sieu-Cap-VIP-Pro@users.noreply.github.com>
2022-06-06 21:57:41 +0700

    Thai

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { getDefaultMiddleware } from "@reduxjs/toolkit";
 import BrandReducer from "../features/admin/brandManagement/brandSlice";
-import AuthReducer from "../features/admin/auth/authSlice"
+import AuthReducer from "../features/admin/auth/authSlice";
 import ProductsReducer from "../features/admin/ProductManagement/ProductSlice";
+import HomeReducer from "../features/customer/home/HomeSlice";
 
 export const store = configureStore({
   reducer: {
     auth: AuthReducer,
     products: ProductsReducer,
     brand: BrandReducer,
+    home: HomeReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
--- src/features/customer/detail/components/DetailProduct.js
+++ src/features/customer/detail/components/DetailProduct.js
@@ -1,57 +1,85 @@
-import React from 'react'
-import { Container, Row, Col, Card, Button } from 'react-bootstrap'
-import './DetailProduct.css'
+import React from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import "./DetailProduct.css";
 
 function DetailProduct() {
-    return (
-        <div>
-            <Row>
-                <Col sm={6}>
-                    <Card >
-                        <Card.Img variant="top" src="https://kingshoes.vn/data/upload/media/jordan-1-mid-tuft-orange-bq6931-802-king-shoes-sneaker-real-hcm-1624273330.jpeg" />
-                        <Card.Body>
-                            <Row>
-                                <Col sm={3}>
-                                    <Card >
-                                        <Card.Img variant="top" src="https://kingshoes.vn/data/upload/media/jordan-1-mid-tuft-orange-bq6931-802-king-shoes-sneaker-real-hcm-1624273330.jpeg" />
-                                    </Card>
-                                </Col>
-                                <Col sm={3}>
-                                    <Card >
-                                        <Card.Img variant="top" src="https://kingshoes.vn/data/upload/media/jordan-1-mid-tuft-orange-bq6931-802-king-shoes-sneaker-real-hcm-1624273330.jpeg" />
-                                    </Card>
-                                </Col>
-                                <Col sm={3}>
-                                    <Card >
-                                        <Card.Img variant="top" src="https://kingshoes.vn/data/upload/media/jordan-1-mid-tuft-orange-bq6931-802-king-shoes-sneaker-real-hcm-1624273330.jpeg" />
-                                    </Card>
-                                </Col>
-                                <Col sm={3}>
-                                    <Card >
-                                        <Card.Img variant="top" src="https://kingshoes.vn/data/upload/media/jordan-1-mid-tuft-orange-bq6931-802-king-shoes-sneaker-real-hcm-1624273330.jpeg" />
-                                    </Card>
-                                </Col>
-                            </Row>
-                        </Card.Body>
-                    </Card>
+  const DetailProduct = useSelector((state) => state.home.DetailProduct);
+
+  return (
+    <div>
+      <Row>
+        <Col sm={6}>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://kingshoes.vn/data/upload/media/jordan-1-mid-tuft-orange-bq6931-802-king-shoes-sneaker-real-hcm-1624273330.jpeg"
+            />
+            <Card.Body>
+              <Row>
+                <Col sm={3}>
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      src="https://kingshoes.vn/data/upload/media/jordan-1-mid-tuft-orange-bq6931-802-king-shoes-sneaker-real-hcm-1624273330.jpeg"
+                    />
+                  </Card>
                 </Col>
-                <Col sm={6}>
-                    <nav class="woocommerce-breadcrumb breadcrumbs">
-                        <a href="home">Trang chủ</a>
-                        <span class="divider">/</span> <a href="nu">Nữ</a>
-                        <span class="divider">/</span> <a href="nu/classic-nu/">Classic</a>
-                    </nav>
-                    <h1 class="product-title product_title entry-title">JORDAN 1 MID TUFT ORANGE</h1>
-                    <p class="ps-product__category">Mã SP: <strong>DD6834 802 - BQ6931 802</strong></p>
-                    <h3 class="ps-product__price"><b class="new-price">5,800,000 đ</b>  </h3>
-                    <div className='detail-buttom'>
-                        <Button variant="primary" className='addToCart'>THÊM VÀO GIỎ HÀNG</Button>
-                        <Button variant="primary" className='buy'>MUA NGAY</Button>
-                    </div>
+                <Col sm={3}>
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      src="https://kingshoes.vn/data/upload/media/jordan-1-mid-tuft-orange-bq6931-802-king-shoes-sneaker-real-hcm-1624273330.jpeg"
+                    />
+                  </Card>
                 </Col>
-            </Row>
-        </div>
-    )
+                <Col sm={3}>
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      src="https://kingshoes.vn/data/upload/media/jordan-1-mid-tuft-orange-bq6931-802-king-shoes-sneaker-real-hcm-1624273330.jpeg"
+                    />
+                  </Card>
+                </Col>
+                <Col sm={3}>
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      src="https://kingshoes.vn/data/upload/media/jordan-1-mid-tuft-orange-bq6931-802-king-shoes-sneaker-real-hcm-1624273330.jpeg"
+                    />
+                  </Card>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={6}>
+          <nav class="woocommerce-breadcrumb breadcrumbs">
+            <a href="home">Trang chủ</a>
+            <span class="divider">/</span> <a href="nu">Nữ</a>
+            <span class="divider">/</span> <a href="nu/classic-nu/">Classic</a>
+          </nav>
+          <h1 class="product-title product_title entry-title">
+            {DetailProduct.product_name}
+          </h1>
+          <p class="ps-product__category">
+            Mã SP: <strong>DD6834 802 - BQ6931 802</strong>
+          </p>
+          <h3 class="ps-product__price">
+            <b class="new-price">5,800,000 đ</b>{" "}
+          </h3>
+          <div className="detail-buttom">
+            <Button variant="primary" className="addToCart">
+              THÊM VÀO GIỎ HÀNG
+            </Button>
+            <Button variant="primary" className="buy">
+              MUA NGAY
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default DetailProduct
+export default DetailProduct;
