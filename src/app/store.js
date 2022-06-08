020d5cec0f5f79faddcd6c9565476fba6487a0c6
LanceNguyen23 <ngtruclan2302@gmail.com>
2022-06-08 09:50:33 +0700

    fix

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "../features/customer/auth/AuthSlice";
-import productsReducer from "../features/customer/products/ProductsSlice";
-import { productsApi } from "../features/customer/products/productApi";
-import cartReducer from "../features/customer/cart/CartSlice";
-import { getDefaultMiddleware } from "@reduxjs/toolkit";
+import CartReducer from "../features/customer/cart/CartSlice";
 import BrandReducer from "../features/admin/brandManagement/brandSlice";
 import ProductsReducer from "../features/admin/ProductManagement/ProductSlice";
 import HomeReducer from "../features/customer/home/HomeSlice";
@@ -11,8 +8,8 @@ import HomeReducer from "../features/customer/home/HomeSlice";
 export const store = configureStore({
   reducer: {
     auth: AuthReducer,
-    products: productsReducer,
-    cart: cartReducer,
+    products: ProductsReducer,
+    cart: CartReducer,
     brand: BrandReducer,
     home: HomeReducer,
   },
